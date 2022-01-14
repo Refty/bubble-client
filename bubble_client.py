2cb69386c4b85c825bbe0fa8d0855808d4913523
Guillaume Gelin <guillaume@refty.co>
2022-01-14 19:10:32 +0100

    Make data fetching more robust with a Cursor class

--- bubble_client.py
+++ bubble_client.py
@@ -2,6 +2,45 @@ from httpx import AsyncClient
 from thingy import NamesMixin, Thingy, classproperty
 
 
+class Cursor:
+    def __init__(self, cls, params):
+        self.cls = cls
+        self.params = params
+        self.index = self.params.get("cursor", 0)
+        self.page = None
+
+    async def _get_page(self):
+        self.params["cursor"] = self.index
+
+        async with AsyncClient(base_url=self.cls.base_url) as client:
+            response = await client.get(
+                f"/api/1.1/obj/{self.cls.typename}",
+                params=self.params,
+                headers=self.cls._headers,
+            )
+            response.raise_for_status()
+        return response.json()["response"]
+
+    @property
+    def page_index(self):
+        return self.index - self.page["cursor"]
+
+    def __aiter__(self):
+        return self
+
+    async def __anext__(self):
+        if not self.page or self.page_index == self.page["count"]:
+            self.page = await self._get_page()
+
+        try:
+            bubble_object = self.page["results"][self.page_index]
+        except IndexError:
+            raise StopAsyncIteration
+
+        self.index += 1
+        return self.cls(bubble_object)
+
+
 class BubbleThing(NamesMixin, Thingy):
     _base_url = None
     _headers = {}
@@ -33,27 +72,8 @@ class BubbleThing(NamesMixin, Thingy):
         return cls._typename or "_".join(cls.names)
 
     @classmethod
-    async def get(cls, params=None):
-        if params is None:
-            params = {}
-
-        async def _get_page(params):
-            async with AsyncClient(base_url=cls.base_url) as client:
-                response = await client.get(
-                    f"/api/1.1/obj/{cls.typename}",
-                    params=params,
-                    headers=cls._headers,
-                )
-                response.raise_for_status()
-            return response.json()["response"]
-
-        params.setdefault("cursor", 0)
-        while page := await _get_page(params):
-            for bubble_object in page["results"]:
-                yield cls(bubble_object)
-            if not page["remaining"]:
-                break
-            params["cursor"] = page["cursor"] + page["count"]
+    def get(cls, **params):
+        return Cursor(cls, params)
 
 
 configure = BubbleThing.configure

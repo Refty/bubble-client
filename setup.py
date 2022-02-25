3c70d36f7da5b127045924bdb2d2980d7956131a
Guillaume Gelin <guillaume@refty.co>
2022-02-25 15:01:57 +0100

    Release 0.6.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def get_description():
 
 setup(
     name="bubble-client",
-    version="0.6.1",
+    version="0.6.2",
     url="https://github.com/Refty/bubble-client",
     author="Guillaume Gelin",
     author_email="guillaume@refty.co",
